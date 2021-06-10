2f259347732c6a159f1473c19c5195e48162aa5e
David Pikas <david@pikas.se>
2021-06-10 13:49:25 +0200

    Problems with compiling with cargo fuzz

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#[macro_use]
+mod parser_wrapper;
+pub use parser_wrapper::parse;
+
+#[macro_use]
+mod context_arbitrary;
+
+#[macro_use]
+mod context;
+pub use context::Context;
+
+#[macro_use]
+mod semantics;
+
+mod syn_arbitrary;
+pub use syn_arbitrary::make_wrapped_file;
+pub use context_arbitrary::*;
+pub use syn_arbitrary::*;
