f9c9dc2fe690c422c132aa06d769e45af9052302
David Pikas <david@pikas.se>
2021-06-11 17:09:51 +0200

    You can now run a variable number of tests from the command line

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use arbitrary::Unstructured;
 use quote::quote;
 use rand::{distributions, prelude::Distribution, thread_rng, Rng};
+use std::env::Args;
 use std::process::Command;
-use std::{env, fs, str, thread};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::{env, fs, str, thread, io::Read};
 
 mod parser_wrapper;
 use parser_wrapper::parse;
 
 #[macro_use]
 mod context_arbitrary;
-use context::Context;
+use context_arbitrary::GenerationError;
 
 #[macro_use]
 mod context;
+use context::Context;
 
 #[macro_use]
 mod semantics;
@@ -23,31 +27,18 @@ use syn_arbitrary::make_wrapped_file;
 use crate::syn_arbitrary::WrappedFile;
 
 fn main() -> Result<(), MainError> {
-    let semantically_correct: bool = Some(String::from("syntax")) != env::args().nth(1);
-    let code_str: String;
 
-    let wrapped_ast: syn_arbitrary::WrappedFile =
-        with_stack_space_of(64 * 1024 * 1024, move || {
-            let mut rng = thread_rng();
-            let len = rng.gen_range(100000..1000000);
-            let data: Vec<u8> = distributions::Standard
-                .sample_iter(&mut rng)
-                .take(len)
-                .collect();
-            
-            let mut u = Unstructured::new(&data);
-            let mut ctx = Context::make_context(semantically_correct);
-            make_wrapped_file(&mut ctx, &mut u)
-        })?;
-    let WrappedFile(ast) = wrapped_ast;
-    let ast_string = format!("{:#?}", ast);
+    let flags = parse_args(env::args());
+
     let ast_filename = "./output_files/ast.rs";
-    fs::write(ast_filename, &ast_string).expect("Failed writing file");
-    code_str = quote!(#ast).to_string();
-    println!("{}", code_str);
     let filename = "./output_files/generated_code.rs";
-    fs::write(filename, &code_str).expect("Failed writing file");
-    if semantically_correct {
+
+    if flags.use_semantics {
+        let ast = gen_code(flags.use_semantics)?;
+        let ast_string = format!("{:#?}", ast);
+        fs::write(ast_filename, &ast_string).expect("Failed writing file");
+        let code_str = quote!(#ast).to_string();
+        fs::write(filename, &code_str).expect("Failed writing file");
         let compilation_output = Command::new("rustc")
             .arg(filename)
             .arg("--allow").arg("warnings")
@@ -57,7 +48,38 @@ fn main() -> Result<(), MainError> {
         println!("{}", str::from_utf8(&compilation_output.stdout).unwrap());
         println!("{}", str::from_utf8(&compilation_output.stderr).unwrap());
     } else {
-        parse(filename)?;
+        let is_interrupeted: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+        let r = is_interrupeted.clone();
+        ctrlc::set_handler(move || {
+            r.store(true, Ordering::SeqCst)
+        }).expect("Error setting the Ctl-C handler");
+
+        let mut count = 0;
+        while !is_interrupeted.load(Ordering::SeqCst) && match flags.repeat {
+            Repeat::N(n) => n > count,
+            Repeat::Forever => true
+        } {
+            count += 1;
+            let ast = gen_code(flags.use_semantics)?;
+            let ast_string = format!("{:#?}", ast);
+            fs::write(ast_filename, &ast_string).expect("Failed writing file");
+            let code_str = quote!(#ast).to_string();
+            fs::write(filename, &code_str).expect("Failed writing file");
+
+            let buf = gag::BufferRedirect::stderr().unwrap();
+            parse(filename)?;
+            let mut output = String::new();
+            buf.into_inner().read_to_string(&mut output).unwrap();
+
+            if !output.is_empty() {
+                let timestamp = chrono::Utc::now().format("%Y-%m-%d:%H:%M:%S%.f");
+                fs::copy(filename, format!("possible_bugs/{}.rs", timestamp)).expect("Failed writing to file");
+                fs::copy(ast_filename, format!("possible_bugs/{}_ast.rs", timestamp)).expect("Failed writing to file");
+                fs::write(format!("possible_bugs/{}_err.txt", timestamp), output.clone()).expect("Failed writing to file");
+                println!("({}):\n{}", timestamp, output);
+            }
+        }
+        println!("Finished {} tests!", count);
     };
     return Ok(())
 }
@@ -88,4 +110,47 @@ impl From<parser_wrapper::ParserError> for MainError {
     fn from(e: parser_wrapper::ParserError) -> Self {
         MainError::ParserError(e)
     }
+}
+
+enum Repeat { N(usize), Forever }
+struct Flags {
+    repeat: Repeat,
+    use_semantics: bool
+}
+
+fn parse_args(mut args: Args) -> Flags {
+    let mut flags = Flags {
+        repeat: Repeat::N(1),
+        use_semantics: true,
+    };
+    while let Some(arg) = args.next() {
+        if arg == "syntax" { flags.use_semantics = false }
+        else if arg == "-n" || arg == "--number_of_iterations" {
+            let n = args.next().expect("Expected value for argument")
+                        .parse().expect("Error while parsing argument");
+            flags.repeat = Repeat::N(n);
+        }
+        else if arg == "-r" || arg == "--repeat" {
+            flags.repeat = Repeat::Forever;
+        }
+        
+    }
+    return flags;
+}
+
+fn gen_code(use_semantics: bool) -> Result<syn::File, GenerationError> {
+    let WrappedFile(ast): syn_arbitrary::WrappedFile =
+        with_stack_space_of(64 * 1024 * 1024, move || {
+            let mut rng = thread_rng();
+            let len = rng.gen_range(100000..1000000);
+            let data: Vec<u8> = distributions::Standard
+                .sample_iter(&mut rng)
+                .take(len)
+                .collect();
+            
+            let mut u = Unstructured::new(&data);
+            let mut ctx = Context::make_context(use_semantics);
+            make_wrapped_file(&mut ctx, &mut u)
+        })?;
+    return Ok(ast);
 }
